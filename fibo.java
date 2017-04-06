5dc1484bec81acc227f252903fef94adfe465af0
Aditya Rahmat Mutaqin <adityarahmatmutaqin@gmail.com>
2017-04-06 09:06:28 +0700

    Add files via upload

---
+++ fibo.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rpl;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Praktikan
+ */
+public class Rpl {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      System.out.println("Jumlah Deret Fibonacci:");
+        Scanner inputFibonacci = new Scanner(System.in);
+        
+        int fibonacciCount = inputFibonacci.nextInt();
+        
+        for (int ii=1; ii < fibonacciCount; ii++)
+        {
+            System.out.println("Bilangan Fibonacci: " + methodFibonacci(ii));
+        }
+    }
+    
+    public static int methodFibonacci(int fibonacciInput)
+    {   
+        int x;
+        int n = fibonacciInput;
+        if(n <=0 || n<=1)
+        {
+           return n; 
+        }
+       
+        else
+        {
+            return methodFibonacci(n-2) + methodFibonacci(n-1) ;
+        }
+    }
+    
+}
